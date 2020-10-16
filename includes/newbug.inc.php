f7f52bce17306c37adca827b964486c4fc3973bd
Jorge <jmdragon8@gmail.com>
2020-10-15 22:30:50 -0700

    Create php code to be able to add new bug to the database.

---
+++ includes/newbug.inc.php
@@ -0,0 +1,35 @@
+<?php
+
+if (!isset($_POST['submit-newbug'])) {
+  header("Location: ../portal/newbug");
+  exit();
+}
+else {
+  require 'dbh.inc.php';
+  $bugName = $_POST['bugName'];
+  $bugDescription = $_POST['bugDescription'];
+  $bugCreatedBy = $_POST['createdby'];
+  $bugAssignedTo = $_POST['assignedTo'];
+
+  if (empty($bugName) || empty($bugDescription) || empty($bugCreatedBy)) {
+    header("Location: ../portal/newbug?error=emptyfields&name=".$bugName."&desc=".$bugDescription);
+    exit();
+  }
+  else {
+    $sql = "INSERT INTO bugs (bugName, bugDescription, bugCreatedBy, bugAssignedTo) VALUES (?, ?, ?,?)";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+      header("Location: ../portal/newbug?error=sqlerror");
+      exit();
+    }
+    else {
+      mysqli_stmt_bind_param($stmt, "ssss", $bugName, $bugDescription, $bugCreatedBy, $bugAssignedTo);
+      mysqli_stmt_execute($stmt);
+      header("Location: ../portal/newbug?creation=success");
+      exit();
+    }
+
+  }
+  mysqli_stmt_close($stmt);
+  mysqli_close($conn);
+}
