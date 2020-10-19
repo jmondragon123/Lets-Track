4b826c9597a09b7c0cbc7a6a76b2278cb94fc33d
Jorge <jmdragon8@gmail.com>
2020-10-19 13:46:18 -0700

    Modified SQl query to not show current user. Modified my table to not show the UID

--- portal/views/admin.php
+++ portal/views/admin.php
@@ -2,14 +2,12 @@
 echo '<div class="user-view">';
 echo '<table class="users-Table">
 <tr>
-  <th>User ID</th>
   <th>Username</th>
   <th>E-mail</th>
   <th>User Group</th>
-  <th>Edit</th>
 </tr>';
 
-$sql = "SELECT users.idUsers,users.uidUsers, users.emailUsers,groups.groupName FROM users INNER JOIN groups ON users.userGroup = groups.idGroup;";
+$sql = "SELECT users.idUsers, users.uidUsers, users.emailUsers,groups.groupName FROM users INNER JOIN groups ON users.userGroup = groups.idGroup WHERE users.idUsers !='".$_SESSION['userID']."' ORDER BY users.idUsers DESC;";
 $result = mysqli_query($conn, $sql);
 $resultsCheck = mysqli_num_rows($result);
 if (!$resultsCheck > 0) {
@@ -19,11 +17,9 @@ else {
 while ($row = mysqli_fetch_assoc($result)) {
 
   echo '<tr>
-  <th>'.$row["idUsers"].
-  '</th> <th>'.$row["uidUsers"].
-  '</th> <th>'.$row['emailUsers'].
-  '</th> <th>'.$row['groupName'].
-  '</th> 
+  <th> <a href=user?userID='.$row["idUsers"].'>'.$row["uidUsers"].'</a></th>
+  <th>'.$row['emailUsers'].'</th>
+  <th>'.$row['groupName'].'</th>
   </tr>';
 }
 }
