71809da7d1a8775829f290457d9772b1b7fc62a8
Jorge <jmdragon8@gmail.com>
2020-10-27 20:11:28 -0700

    Fixed whitespaces and minor changes in code

--- portal/header.php
+++ portal/header.php
@@ -2,24 +2,24 @@
   session_start();
   include_once '../includes/dbh.inc.php';
   include_once '../includes/functions.php';
- ?>
- <?php  if(time() > $_SESSION['expire']){
-   session_destroy();
+?>
+<?php  if(time() > $_SESSION['expire']){
+  session_destroy();
     session_write_close();
     session_unset();
     $_SESSION = array();
- }
- else {
-   $_SESSION['expire'] = time() + 30*60;
- }
- ?>
+}
+else {
+  $_SESSION['expire'] = time() + 30*60;
+}
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta name=viewport content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type = "text/css" href = "../styles/portalstyles.css">
-     <script src="../scripts/myscripts.js"></script>
+    <script src="../scripts/myscripts.js"></script>
     <title>Let's Track</title>
   </head>
   <body>
@@ -27,7 +27,7 @@
       <div>
       <nav>
         <ul class="left-form">
-          <li><a href="portal">Home</a></li>
+          <li><a href="agent">Home</a></li>
           <li><a href="open">Open</a></li>
           <li><a href="progress">In progress</a></li>
           <li><a href="closed">Closed</a></li>
@@ -46,11 +46,11 @@
           </form></li>
         </ul>
       </nav>
-     </div>
+    </div>
 
-     <?php
-     if (!isset($_SESSION['userID'])) {
-       header("Location: ../index");
-     }
-     ?>
+    <?php
+    if (!isset($_SESSION['userID'])) {
+      header("Location: ../index");
+    }
+    ?>
     </header>
