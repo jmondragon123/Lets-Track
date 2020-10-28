74191a0c50b05c3ffa2e75f88fd39a188e43d781
Jorge <jmdragon8@gmail.com>
2020-10-27 20:45:32 -0700

    Adding ability to remove users.

---
+++ portal/removeuser.php
@@ -0,0 +1,18 @@
+<?php
+  require "header.php"
+  ?>
+
+<main>
+  <div class="container">
+    <div class="cont-info">
+      <h1 class="title">Remove user</h1>
+
+
+    </div>
+  </div>
+</main>
+
+
+<?php
+  require "footer.php"
+?>
